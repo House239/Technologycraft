5e0726bdd29ad17f5fed7ef76783d78217f46665
House239 <elias.oa.kola@gmail.com>
2025-04-28 12:45:07 +0300

    Delete CommonProxy.java

--- CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package com.House239.technologycraft;
-
-import net.minecraft.item.Item;
-
-//public class CommonProxy{
-//    public void registerItemRenderer(Item item, int meta, String id){
-//    }
-//}
